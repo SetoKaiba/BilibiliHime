160f52b64bbbdbda8858e44fb08c5b4e8219f126
Seto <61304189@qq.com>
2020-03-07 23:27:17 +0800

    Fixed BilibiliHime

--- Assets/Scripts/BilibiliHime/BilibiliHime.cs
+++ Assets/Scripts/BilibiliHime/BilibiliHime.cs
@@ -1,18 +1,22 @@
-﻿using UnityEngine;
+﻿using CI.TaskParallel;
+using UnityEngine;
 
 public class BilibiliHime : SingletonUtil<BilibiliHime>
 {
     public Animator animator;
-    
+
     // Start is called before the first frame update
     void Start()
     {
         animator = GetComponent<Animator>();
+        BilibiliLiveNetty.Instance.onDanmakuMessage = (uname, message) =>
+        {
+            UnityTask.RunOnUIThread(() => animator.SetTrigger("DAMAGED00"));
+        };
     }
 
     // Update is called once per frame
     void Update()
     {
-        
     }
 }
